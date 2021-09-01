50ba7eb95c6230595c8516b84462a4a4547a2739
danvran <85077200+danvran@users.noreply.github.com>
2021-09-01 16:54:37 +0200

    update to new namespace

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
-import pyhtml
+from pyhtmlgen import generators as htmlgen
 
 import numpy as np
 import plotly.graph_objects as go
 
-website = pyhtml.start("pyhtml test site")
+website = htmlgen.start("pyhtml test site")
 
-website += pyhtml.h1("This is a title")
+website += htmlgen.h1("This is a title")
 
-website += pyhtml.para("This is a paragraph")
+website += htmlgen.para("This is a paragraph")
 
 N = 1000
 t = np.linspace(0, 10, 100)
@@ -15,7 +15,7 @@ y = np.sin(t)
 
 fig = go.Figure(data=go.Scatter(x=t, y=y, mode='markers'))
 
-website += pyhtml.plotly_figure(fig, 'id_01')
+website += htmlgen.plotly_figure(fig, 'id_01')
 
 with open('Testwebsite.html', 'w') as f:
     f.write(website)
